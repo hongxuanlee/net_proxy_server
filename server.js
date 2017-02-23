722c2bc72c65a33b291ba4134cb4526e18ee4365
hongxuanlee <lihongxuan213@gmail.com>
2017-02-23 19:43:33 +0800

    handle message

--- server.js
+++ server.js
@@ -1,33 +1,71 @@
 'use strict';
 
 let net = require('net');
+let remain = 0;
+let remainData = '';
 
 let server = net.createServer((c) => {
-    console.log('client connected');
+  console.log('client connected');
 
-    console.log(c.localAddress, c.localPort,
-        c.remoteAddress, c.remotePort);
+  console.log(c.localAddress, c.localPort,
+    c.remoteAddress, c.remotePort);
 
-    c.on('end', () => {
-        console.log('client disconnected');
-    });
+  c.on('end', () => {
+    console.log('client disconnected');
+  });
 
-    c.on('data', (chunk) => {
-        
-        console.log('----------data----------');
-        //console.log(chunk.toString('utf-8'));
-        console.log(chunk);
-        c.write('7');
-    });
-
-    c.write('7');
-    
+  c.on('data', (chunk) => {
+    console.log('----------data----------');
+    console.log(chunk);
+    if (remain === 0) {
+      let len = chunk.slice(0, 4).readInt32LE();
+      console.log(len);
+      let str = chunk.slice(4);
+      console.log(str);
+      handleBuffer(str, len - 4, c);
+    } else {
+      let str = remainData + chunk;
+      handleBuffer(str, remain, c);
+    }
+  });
 });
 
+function handleBuffer(chunk, totalLen, c) {
+  let chunkLen = chunk.length
+  console.log(chunkLen);
+  if (totalLen - chunkLen === 0) {
+    let data = JSON.parse(chunk.toString('ascii'));
+    console.log('receive message:')
+    console.log(data);
+    handleMessage(data, c);
+  } else {
+    remainData = chunk;
+    remain = totalLen
+  }
+}
+
+function handleMessage(data, c) {
+  const buf = Buffer.allocUnsafe(4);
+  if (data.command === 6) {
+    let str = JSON.stringify({
+      'command': 7,
+      'result-code': 0,
+      'identifier': data.identifier
+    });
+    let buStr = Buffer.from(str, 'utf-8')
+    console.log(buStr);
+    buf.writeInt32LE(buStr.length + 4, 0);
+    let message = Buffer.concat([buf, buStr]);
+    console.log('send message:');
+    console.log(message);
+    c.write(message);
+  }
+}
+
 server.on('error', (err) => {
-    console.log(err);
+  console.log(err);
 });
 
 server.listen(8124, () => {
-    console.log('server bound at 8124');
+  console.log('server bound at 8124');
 });
